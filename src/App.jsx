c2a974328516fb0b290714528b1cd4d3ca3a4647
David <78326757+daols1@users.noreply.github.com>
2023-04-01 13:10:45 +0100

    Added some packages that we would need

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,6 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-        {JSON.stringify(import.meta.env.VITE_Firebase_Api_Key)}
       </p>
     </div>
   )
