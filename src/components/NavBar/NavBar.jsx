686cf1973c1149c43931a304f519e18c6eff5c91
David <78326757+daols1@users.noreply.github.com>
2023-04-03 02:17:28 +0100

    Added the routes and Shared layout

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
-import Logo from '../../assets/Logo.png'
+import Logo from '../../assets/logo.png'
 import {BiMenuAltRight} from 'react-icons/bi'
 import {IoMdClose} from 'react-icons/io'
 import { motion } from 'framer-motion'
